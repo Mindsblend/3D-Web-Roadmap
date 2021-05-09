cc3a9acd9a4a3853350526766e5ff8709c0bc58c
Chamepp <ashkanebtekari@gmail.com>
2021-05-09 15:20:43 -0500

    Started Lesson-Six

---
+++ Lesson-Six/three.js-master/test/unit/src/geometries/CircleGeometry.tests.js
@@ -0,0 +1,54 @@
+/* global QUnit */
+
+import { runStdGeometryTests } from '../../utils/qunit-utils';
+import { CircleGeometry, CircleBufferGeometry } from '../../../../src/geometries/CircleGeometry';
+
+export default QUnit.module( 'Geometries', () => {
+
+	QUnit.module( 'CircleGeometry', ( hooks ) => {
+
+		var geometries = undefined;
+		hooks.beforeEach( function () {
+
+			const parameters = {
+				radius: 10,
+				segments: 20,
+				thetaStart: 0.1,
+				thetaLength: 0.2
+			};
+
+			geometries = [
+				new CircleGeometry(),
+				new CircleGeometry( parameters.radius ),
+				new CircleGeometry( parameters.radius, parameters.segments ),
+				new CircleGeometry( parameters.radius, parameters.segments, parameters.thetaStart ),
+				new CircleGeometry( parameters.radius, parameters.segments, parameters.thetaStart, parameters.thetaLength ),
+					new CircleBufferGeometry(),
+			];
+
+		} );
+
+		// INHERITANCE
+		QUnit.todo( "Extending", ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+		// INSTANCING
+		QUnit.todo( "Instancing", ( assert ) => {
+
+			assert.ok( false, "everything's gonna be alright" );
+
+		} );
+
+		// OTHERS
+		QUnit.test( 'Standard geometry tests', ( assert ) => {
+
+			runStdGeometryTests( assert, geometries );
+
+		} );
+
+	} );
+
+} );
