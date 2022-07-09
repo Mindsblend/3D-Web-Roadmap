739db79f9f57e3195b0da267092e2be5999cacfa
System Administrator <ashkanebtekari@gmail.com>
2022-07-09 16:24:01 +0430

    completed lesson two

---
+++ 02-Basic-Scene/index.js
@@ -0,0 +1,33 @@
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMaterial = new THREE.MeshBasicMaterial({
+    color: '#ff0000'
+})
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
+scene.add(cubeMesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
